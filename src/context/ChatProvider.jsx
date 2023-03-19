92863fd4cfab6b704f98352f8ce9d37c489438e4
acarov1407 <carovelandia07@gmail.com>
2023-03-19 16:10:27 -0500

    Adding console testing

--- src/context/ChatProvider.jsx
+++ src/context/ChatProvider.jsx
@@ -27,6 +27,8 @@ function ChatProvider({ children }) {
 
     useEffect(() => {
         loadChat();
+        console.log(socket);
+        console.log(import.meta.env.VITE_BACKEND_URL)
     }, []);
 
     useEffect(() => {
